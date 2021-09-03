928e136a5b95618b5dbfd4a20317122a3346be31
Fabrício de Medeiros <fabriciodemedeiros@icloud.com>
2021-09-02 21:06:36 -0300

    creating getPessoa method

--- unidadeI/classes/Pessoa.php
+++ unidadeI/classes/Pessoa.php
@@ -1,8 +1,15 @@
 <?php 
-class Pessoa {
-    public $nome;
-    public $email;
-    public $telefone;
-    public $dataNascimento;
+    class Pessoa {
+        public $nome;
+        public $email;
+        public $telefone;
+        public $dataNascimento;
+    
+        public function getPessoa(){
+            echo "<br>Nome: " . $this->nome;
+            echo "<br>E-maeil: " . $this->email;
+            echo "<br>Telefone: " . $this->telefone;
+            echo "<br>Data Nasciomento: " . $this->dataNascimento;
+        }
 }
 ?>
--- unidadeI/controlePessoa.php
+++ unidadeI/controlePessoa.php
@@ -9,9 +9,6 @@ $pessoa->telefone = "44 3027-9390";
 $pessoa->dataNascimento = "2020-08-14";
 
 echo "<div><strong>Dados da Pessoa</strong>";
-echo "Nome: " . $pessoa->nome;
-echo "E-mael: " . $pessoa->email;
-echo "Telefone: " . $pessoa->telefone;
-echo "Data Nascimento: " . $pessoa->dataNascimento;
+$pessoa->getPessoa();
 echo "</div>";
 ?>
